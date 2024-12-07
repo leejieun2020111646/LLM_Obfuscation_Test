90f4a98d66501559c0e94a007e0157e40244a3f5
LeeJieun <87838287+leejieun2020111646@users.noreply.github.com>
2024-12-07 15:00:52 +0900

    Create test1_llm_deob.c

---
+++ 1207/test1_llm_deob.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n == 1 || n == 0) 
+        return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int num;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    printf("Fibonacci number of %d: %d\n", num, fibonacci(num));
+    return 0;
+}
