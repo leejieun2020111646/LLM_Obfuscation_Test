02fada093695061e57c81657fe11e00f64b5590d
LeeJieun <87838287+leejieun2020111646@users.noreply.github.com>
2024-12-10 00:33:56 +0900

    Update sample_sort_original.c

--- paired_samples_tigress/sample_sort_original.c
+++ paired_samples_tigress/sample_sort_original.c
@@ -1,25 +1,7 @@
-/**
- * @file
- * @brief Sorting of array list using [bead
- * sort](https://en.wikipedia.org/wiki/Bead_sort)
- */
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
- * @addtogroup sorting Sorting algorithms
- * @{
- */
-/** Create easy access of elements from a 2D matrix stored in memory as a 1D
- * array
- */
 #define BEAD(i, j) beads[i * max + j]
 
-/**
- * Displays the array, passed to this method
- * @param [in] arr array to display
- * @param [in] n number of elements in the array
- */
 void display(const int *arr, int n)
 {
     for (int i = 0; i < n; i++)
@@ -30,10 +12,6 @@ void display(const int *arr, int n)
     printf("\n");
 }
 
-/** This is where the sorting of the array takes place
- * @param [in,out] a array to be sorted
- * @param [in] len Array Size
- */
 void bead_sort(int *a, size_t len)
 {
     int i, j, max, sum;
@@ -45,22 +23,20 @@ void bead_sort(int *a, size_t len)
 
     beads = calloc(1, max * len);
 
-    /* mark the beads */
     for (i = 0; i < len; i++)
-        for (j = 0; j < a[i]; j++) BEAD(i, j) = 1;
+        for (j = 0; j < a[i]; j++)
+            BEAD(i, j) = 1;
 
     for (j = 0; j < max; j++)
     {
-        /* count how many beads are on each post */
         for (sum = i = 0; i < len; i++)
         {
             sum += BEAD(i, j);
             BEAD(i, j) = 0;
         }
-        /* mark bottom sum beads */
-        for (i = len - sum; i < len; i++) BEAD(i, j) = 1;
+        for (i = len - sum; i < len; i++)
+            BEAD(i, j) = 1;
     }
-
     for (i = 0; i < len; i++)
     {
         for (j = 0; j < max && BEAD(i, j); j++)
@@ -69,14 +45,12 @@ void bead_sort(int *a, size_t len)
     }
     free(beads);
 }
-/** @} */
 
-/** Main function */
 int main(int argc, const char *argv[])
 {
     int n;
     printf("Enter size of array:\n");
-    scanf("%d", &n);  // E.g. 8 1 2 3
+    scanf("%d", &n);
 
     printf("Enter the elements of the array\n");
     int i;
