b3268dbb336ba429a60a262cb54903f708fd1712
LeeJieun <87838287+leejieun2020111646@users.noreply.github.com>
2024-12-10 10:59:42 +0900

    Update sample_fibo_deobfuscated.c

--- paired_samples_data/sample_fibo_deobfuscated.c
+++ paired_samples_data/sample_fibo_deobfuscated.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
+#include<stdio.h> 
+int fibonacci(int n) 
+{ 
+if (n==1||n==0)
+	return n; 
+return fibonacci(n-1) + fibonacci(n-2); 
+} 
 
-int fibonacci(int n) {
-    if (n == 1 || n == 0) 
-        return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
-}
-
-int main() {
-    int num;
-    printf("Enter the number: ");
-    scanf("%d", &num);
-    printf("Fibonacci number of %d: %d\n", num, fibonacci(num));
-    return 0;
+int main () 
+{ 
+  int num;
+  printf("Enter the number");
+  scanf("%d",&num); 
+  printf("Fibonacci number of %d : %d\n", num, fibonacci(num)); 
+  return 0; 
 }
